2fd557d43e3645b65f717394ca10eed3a2630684
elizabeth.orta <beth.orta@asurion.com>
2022-07-29 13:31:46 -0500

    Day 4 code

---
+++ Day 4/banker_roulette_coding_challenge.py
@@ -0,0 +1,11 @@
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+name = random.choice(names)
+print(name + " is going to buy the meal today!")
